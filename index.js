e34e01aa6f9d3b051c49bd541318a4a79ab55b99
burhanfauzisalam <burhanfauzisalam@gmail.com>
2023-08-04 10:06:46 +0700

    change response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hey this is my API running ?");
+  res.status(200).json({ message: "Server started" });
 });
 
 app.listen(1000, () => {
