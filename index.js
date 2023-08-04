d8f4081517a1d844fd2849b37d72ad80f177ca73
burhanfauzisalam <burhanfauzisalam@gmail.com>
2023-08-04 10:45:00 +0700

    setting connection

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+require("dotenv").config();
 
 // Koneksi ke MongoDB
 async function connectToMongoDB() {
