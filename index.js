795c7e3e79bb016022d52ea5e05a628c091adfd1
burhanfauzisalam <burhanfauzisalam@gmail.com>
2023-08-04 10:30:34 +0700

    setting koneksi

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
+// app.js (atau nama file yang sesuai)
 const express = require("express");
 const mongoose = require("mongoose");
 
-const mongoString = process.env.MONGODB_URI;
-mongoose.connect(mongoString);
-const database = mongoose.connection;
-database.on("error", (error) => {
-  console.log(error);
-});
-
-database.once("connected", () => {
-  console.log("Database Connected");
-});
-
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Server started" });
+// Koneksi ke MongoDB
+async function connectToMongoDB() {
+  try {
+    const uri = process.env.MONGODB_URI;
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB!");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
+}
+
+connectToMongoDB();
+
+// Definisikan model dan skema Mongoose (contoh)
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
 });
 
-app.listen(1000, () => {
-  console.log(`Server Started}`);
+const User = mongoose.model("User", UserSchema);
+
+// Tambahkan rute untuk mengakses data dari MongoDB
+app.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-module.exports = app;
+// Tambahkan rute lain sesuai kebutuhan aplikasi Anda
+
+// Jalankan server Express
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
